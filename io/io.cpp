6940341cf594d3263d417ac9f433df40082aa211
Khalid Al-Kooheji <khalid.alkooheji@gmail.com>
2013-08-16 09:35:55 +0300

    small bug in io

--- io/io.cpp
+++ io/io.cpp
@@ -9,6 +9,8 @@ void ReadWholeFileBinary(const char* filename, uint8_t** data_pointer, size_t& d
   *data_pointer = nullptr;
   data_length = 0;
   FILE* fp = fopen(filename,"rb");
+  if (fp == null)
+    return;
   fseek(fp,0,SEEK_END);
 //#ifdef _WIN64
   //data_length = _ftelli64(fp);
